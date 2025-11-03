cee643d9c4f1aa43e47e546a23ebfd9f969e4afd
manar saighi <manarsaighi2003@icloud.com>
2025-11-03 11:47:06 +0000

    fix database

--- places/models.py
+++ places/models.py
@@ -12,8 +12,28 @@ class KnowledgePlace(models.Model):
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     address = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    geometry = models.PointField(srid=4326) 
-   
+    geometry = models.PointField(srid=4326)
+
+    class Meta:
+        db_table = 'places_knowledgeplace'
+        verbose_name = "Knowledge Place"
+        verbose_name_plural = "Knowledge Places"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    path = models.LineStringField(srid=4326)
+
+    class Meta:
+        db_table = 'routes'
+        verbose_name = "Route"
+        verbose_name_plural = "Routes"
+        ordering = ['name']
 
     def __str__(self):
         return self.name
